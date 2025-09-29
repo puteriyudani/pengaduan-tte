b079fac63283752b538e63fe99a7c3c7658154d3
puteriyudani <puteriyudani@gmail.com>
2025-09-29 12:02:38 +0700

    logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -340,7 +340,14 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a href="{{ route('logout') }}" class="btn btn-primary"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
